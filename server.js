913a6112b242a8fd2607869a58352104b7255eb5
imanmansour86 <imanmansour86@gmail.com>
2022-02-01 13:01:03 -0800

    establish first get route for movies

--- server.js
+++ server.js
@@ -1,2 +1,33 @@
-const mysql = require('mysql2');
-const express = require('express');
+const mysql = require("mysql2");
+const express = require("express");
+
+//configure the app to use express and port
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+//set express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//connect to db
+var connection = mysql.createConnection(
+  {
+    host: "127.0.0.1",
+    // MySQL username,
+    user: "root",
+    // MySQL password
+    password: "",
+    database: "movies_db",
+  },
+  console.log(`Connected to the empoloyeeTracker database.`)
+);
+
+app.get("/api/movies", (req, res) => {
+  connection.query(`SELECT * from movies;`, function (err, results) {
+    console.log(results);
+  });
+});
+//configure app to listen on specified port above
+app.listen(PORT, () => {
+  console.log("App listening on PORT " + PORT);
+});
